e763a7e2f7853bac8cccbd28692b81d38e9dd0fb
Justin <justin.tosh.14@ucl.ac.uk>
2024-06-07 14:40:00 +0100

    a little more JS work in progress

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 console.log("test");
 
-//The all important cookie count variable
+//The all important kitty count variable
 
 let kittyCount = 0;
 
 // cookies per second could start at 0 and be initialised on first click? stretch goal
+//This variable will track the passive kitty generation
 let cps = 1;
+
+//From Manny's template: make an array variable ready to accept an array of objects from the API
+let shopItems = [];
 
 const kittyButton = document.getElementById("cookie-button");
 
@@ -17,7 +21,7 @@ kittyButton.addEventListener("click", function () {
 });
 
 function catIncrement() {
-  kittyCount++; //this function will change based on upgrades... may not be a pure function because of that
+  kittyCount += cps; //this function will change based on upgrades... may not be a pure function because of that
 }
 
 function updateCounter() {
@@ -25,4 +29,30 @@ function updateCounter() {
   kittyCounter.textContent = kittyCount;
 }
 
-updateCounter();
+//setting the passive kitty generation to update every second by the amount in
+let myInterval = setInterval(function () {
+  //   console.log("I repeat myself every second");
+  catIncrement();
+  updateCounter();
+}, 1000);
+
+const upgradeContainer = document.querySelector("#upgrade-container");
+
+//fecthing the upgrades from API, basing this a lot on Manny's demo
+
+async function getShopAPI() {
+  const receivedData = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  console.log(receivedData);
+  const data = await receivedData.json();
+  console.log(data);
+  const wrangledData = data["0"];
+  console.log(wrangledData);
+  return wrangledData;
+}
+
+let testAPI = getShopAPI();
+console.log(testAPI);
+
+//don't forget a "not enough cookies notice"
