ff8b87031ec879a93155d5b1f517c8f24e10feed
Justin <justin.tosh.14@ucl.ac.uk>
2024-06-08 14:41:08 +0100

    got save and load to work?

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let kittyCount = 0;
 
 // cookies per second could start at 0 and be initialised on first click? stretch goal
 //This variable will track the passive kitty generation
-let cps = 1;
+let kps = 1;
 
 //From Manny's template: make an array variable ready to accept an array of objects from the API
 let shopItems = [];
@@ -21,7 +21,7 @@ kittyButton.addEventListener("click", function () {
 });
 
 function catIncrement() {
-  kittyCount += cps; //this function will change based on upgrades... may not be a pure function because of that
+  kittyCount += kps; //this function will change based on upgrades... may not be a pure function because of that
 }
 
 function updateCounter() {
@@ -34,6 +34,7 @@ let myInterval = setInterval(function () {
   //   console.log("I repeat myself every second");
   catIncrement();
   updateCounter();
+  storeScore();
 }, 1000);
 
 const upgradeContainer = document.querySelector("#upgrade-container");
@@ -59,3 +60,30 @@ console.log(testAPI);
 
 //need to make localstorage function - this can go in the iteration function to update every second
 //need to make reset button - might not need to update localstorage here
+
+//locastorage test
+// Example number variable
+
+// Store the numbers as a string in local storage under keys = savedcount and
+function storeScore() {
+  localStorage.setItem("savedcount", kittyCount.toString());
+  localStorage.setItem("savedkps", kps.toString());
+  // To retrieve the number later
+
+  //   console.log(retrievedCount);
+}
+
+//not sure if I need to do some sort of if statement to check whether localstorage has been read, or if this will automatically just run once.
+function restoreScore() {
+  let retrievedCountString = localStorage.getItem("savedcount");
+  let retrievedKpsString = localStorage.getItem("savedkps");
+  // Convert the string back to a number
+  let retrievedCount = Number(retrievedCountString);
+  let retrievedKps = Number(retrievedCountString);
+  //now to save the kitty per second
+  //used .toString since can't save non-strings to the locastorage and this isn't a form.
+  kittyCount = retrievedCount;
+  kps = retrievedKps;
+}
+
+restoreScore();
